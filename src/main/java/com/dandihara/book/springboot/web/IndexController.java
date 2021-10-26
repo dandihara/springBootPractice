d22788312cd80098bda4e41079cd4851479e0b95
bong <bmham@explainalpha.com>
2021-10-26 18:39:23 +0900

    5장까지 완료 / 기본 게시판 형식 완료

--- src/main/java/com/dandihara/book/springboot/web/IndexController.java
+++ src/main/java/com/dandihara/book/springboot/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.dandihara.book.springboot.web;
 
+import com.dandihara.book.springboot.config.auth.LoginUser;
+import com.dandihara.book.springboot.config.auth.dto.SessionUser;
 import com.dandihara.book.springboot.service.posts.PostsService;
 
 import com.dandihara.book.springboot.web.dto.PostsResponseDto;
@@ -9,6 +11,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 
 @RequiredArgsConstructor
 @Controller
@@ -17,8 +21,12 @@ public class IndexController {
     private final PostsService postsService;
 
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){
         model.addAttribute("posts",postsService.findAllDesc());
+
+        if (user != null){
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
@@ -34,7 +42,6 @@ public class IndexController {
 
         return "posts-update";
     }
-
 
 
 
